afacd5f0a8bc63805dc18d9af58d081f9a508e8e
Guan Gui <root@guiguan.net>
2022-01-12 22:35:36 +1100

    Add shortcuts with improvements and fixes

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { useRef, useState, useEffect, useCallback } from "react";
-import { Card, TextArea, Button } from "ui-neumorphism";
+import { Card, TextArea, Button, Tooltip } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
 import useSize from "@react-hook/size";
+import useHotkeys from "@reecelucas/react-use-hotkeys";
 import Messages from "../messages/Messages";
 import ErrorAlert from "../error-alert/ErrorAllert";
 import {
@@ -13,6 +14,8 @@ import {
   Message,
 } from "../../generated/graphql";
 import "./Chat.css";
+
+const SEND_SHORTCUT = "Shift+Enter";
 
 export default function Chat({ email }: { email: string }) {
   const textareaParent = useRef(null);
@@ -43,8 +46,13 @@ export default function Chat({ email }: { email: string }) {
   }, [subLoading, data]);
 
   const [msg, setMsg] = useState("");
+  const [uncontrolled, setUncontrolled] = useState(false);
 
   const sendMsg = useCallback(async () => {
+    if (!msg) {
+      return;
+    }
+
     try {
       await sendMessageMutation({
         variables: {
@@ -56,6 +64,11 @@ export default function Chat({ email }: { email: string }) {
       });
 
       setMsg("");
+      // this textarea is buggy and needs to toggle this property in order to sync the value in
+      setUncontrolled(true);
+      setUncontrolled(false);
+      // hack to regain focus
+      document.getElementsByTagName("textarea")[0].focus();
     } catch (error) {
       console.log(error);
     }
@@ -82,6 +95,16 @@ export default function Chat({ email }: { email: string }) {
     [rows, email, deleteMessageMutation]
   );
 
+  const handleSendShortcut = useCallback(
+    (e) => {
+      e.preventDefault();
+      sendMsg();
+    },
+    [sendMsg]
+  );
+
+  useHotkeys(SEND_SHORTCUT, handleSendShortcut);
+
   const error = subError || sendMsgError || deleteMsgError;
 
   return (
@@ -98,12 +121,18 @@ export default function Chat({ email }: { email: string }) {
       <Card flat>
         <div ref={textareaParent}>
           <TextArea
+            autofocus
+            uncontrolled={uncontrolled}
             loading={sendMsgLoading}
             height={80}
             width={width - 110}
             value={msg}
             onChange={({ value }: { value: string }) => setMsg(value)}
-            append={<Button onClick={sendMsg}>Send</Button>}
+            append={
+              <Tooltip content={<div>{SEND_SHORTCUT}</div>}>
+                <Button onClick={sendMsg}>Send</Button>
+              </Tooltip>
+            }
           />
         </div>
       </Card>
--- frontend/src/components/register/Register.tsx
+++ frontend/src/components/register/Register.tsx
@@ -1,81 +1,93 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import {
-    mdiEmailOutline,
-    mdiAccountOutline,
-} from "@mdi/js";
+import { useCallback } from "react";
+import { mdiEmailOutline, mdiAccountOutline } from "@mdi/js";
 import Icon from "@mdi/react";
 import { useHistory } from "react-router-dom";
-import {
-    Card,
-    TextField,
-    Button,
-} from "ui-neumorphism";
+import { Card, TextField, Button } from "ui-neumorphism";
+import useHotkeys from "@reecelucas/react-use-hotkeys";
 import "ui-neumorphism/dist/index.css";
-import ErrorAlert from '../error-alert/ErrorAllert';
+import ErrorAlert from "../error-alert/ErrorAllert";
 import { useRegisterUserMutation } from "../../generated/graphql";
-import "./Register.css"
+import "./Register.css";
+
+const LOGIN_SHORTCUT = "Enter";
 
 export default function Register({
-    email,
-    setEmail,
-    name,
-    setName,
+  email,
+  setEmail,
+  name,
+  setName,
 }: {
-    email: string,
-    setEmail: React.Dispatch<React.SetStateAction<string>>,
-    name: string,
-    setName: React.Dispatch<React.SetStateAction<string>>,
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
 }) {
-    const [registerUserMutation, { loading, error }] = useRegisterUserMutation();
+  const [registerUserMutation, { loading, error }] = useRegisterUserMutation();
 
-    const history = useHistory();
+  const history = useHistory();
 
-    return (
-        <Card
-            loading={loading}
-        >
-            {error && <ErrorAlert>{error.message}</ErrorAlert>}
-            <Card flat
-                css={css`
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    padding: 30px;
-                `}>
-                <TextField
-                    uncontrolled
-                    label="Email"
-                    value={email}
-                    append={<Icon path={mdiEmailOutline} size={1} />}
-                    onChange={({ value }: { value: string }) => setEmail(value)}
-                />
-                <TextField
-                    uncontrolled
-                    label="Name"
-                    value={name}
-                    append={<Icon path={mdiAccountOutline} size={1} />}
-                    onChange={({ value }: { value: string }) => setName(value)}
-                />
-                <Button
-                    onClick={async () => {
-                        try {
-                            await registerUserMutation({
-                                variables: {
-                                    user: {
-                                        email,
-                                        name,
-                                    }
-                                }
-                            })
+  const login = useCallback(async () => {
+    try {
+      await registerUserMutation({
+        variables: {
+          user: {
+            email,
+            name,
+          },
+        },
+      });
 
-                            history.push("/chat?email=" + encodeURIComponent(email) + "&name=" + encodeURIComponent(name));
-                        } catch (error) {
-                            console.error(error);
-                        }
-                    }}
-                >Login</Button>
-            </Card>
-        </Card >
-    )
+      history.push(
+        "/chat?email=" +
+          encodeURIComponent(email) +
+          "&name=" +
+          encodeURIComponent(name)
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }, [registerUserMutation, email, name, history]);
+
+  const handleLoginShortcut = useCallback(
+    (e) => {
+      e.preventDefault();
+      login();
+    },
+    [login]
+  );
+
+  useHotkeys(LOGIN_SHORTCUT, handleLoginShortcut);
+
+  return (
+    <Card loading={loading}>
+      {error && <ErrorAlert>{error.message}</ErrorAlert>}
+      <Card
+        flat
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 30px;
+        `}
+      >
+        <TextField
+          uncontrolled
+          label="Email"
+          value={email}
+          append={<Icon path={mdiEmailOutline} size={1} />}
+          onChange={({ value }: { value: string }) => setEmail(value)}
+        />
+        <TextField
+          uncontrolled
+          label="Name"
+          value={name}
+          append={<Icon path={mdiAccountOutline} size={1} />}
+          onChange={({ value }: { value: string }) => setName(value)}
+        />
+        <Button onClick={login}>Login</Button>
+      </Card>
+    </Card>
+  );
 }
