28b8e6cbd6ab6a13b059e697a385d5ec37ccc69c
Guan Gui <root@guiguan.net>
2022-01-12 15:52:25 +1100

    Fix IM in text input

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,15 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { useRef, useState, useEffect, useCallback } from "react";
-import {
-  Card,
-  TextArea,
-  Button,
-} from "ui-neumorphism";
+import { Card, TextArea, Button } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
-import useSize from '@react-hook/size';
-import Messages from '../messages/Messages';
-import ErrorAlert from '../error-alert/ErrorAllert';
+import useSize from "@react-hook/size";
+import Messages from "../messages/Messages";
+import ErrorAlert from "../error-alert/ErrorAllert";
 import {
   useSubscribeMessagesSubscription,
   useSendMessageMutation,
@@ -18,32 +14,35 @@ import {
 } from "../../generated/graphql";
 import "./Chat.css";
 
-export default function Chat({
-  email,
-}: {
-  email: string,
-}) {
+export default function Chat({ email }: { email: string }) {
   const textareaParent = useRef(null);
-  const [width] = useSize(textareaParent)
+  const [width] = useSize(textareaParent);
 
-  const [rows, setRows] = useState<Message[]>([])
+  const [rows, setRows] = useState<Message[]>([]);
 
-  const { loading: subLoading, error: subError, data } = useSubscribeMessagesSubscription({
+  const {
+    loading: subLoading,
+    error: subError,
+    data,
+  } = useSubscribeMessagesSubscription({
     variables: {
       userId: email,
     },
-  })
+  });
 
-  const [sendMessageMutation, { loading: sendMsgLoading, error: sendMsgError }] = useSendMessageMutation()
+  const [
+    sendMessageMutation,
+    { loading: sendMsgLoading, error: sendMsgError },
+  ] = useSendMessageMutation();
 
   useEffect(() => {
     if (!subLoading && data) {
-      setRows([...rows, data.message])
+      setRows([...rows, data.message]);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [subLoading, data])
+  }, [subLoading, data]);
 
-  const [msg, setMsg] = useState("")
+  const [msg, setMsg] = useState("");
 
   const sendMsg = useCallback(async () => {
     try {
@@ -51,67 +50,61 @@ export default function Chat({
         variables: {
           userId: email,
           msg: {
-            content: msg
-          }
-        }
-      })
+            content: msg,
+          },
+        },
+      });
 
       setMsg("");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }, [email, msg, sendMessageMutation])
+  }, [email, msg, sendMessageMutation]);
 
-  const [deleteMessageMutation, { error: deleteMsgError }] = useDeleteMessageMutation();
+  const [deleteMessageMutation, { error: deleteMsgError }] =
+    useDeleteMessageMutation();
 
-  const deleteMsg = useCallback(async (msgId: string) => {
-    try {
-      await deleteMessageMutation({
-        variables: {
-          userId: email,
-          msgId,
-        }
-      })
+  const deleteMsg = useCallback(
+    async (msgId: string) => {
+      try {
+        await deleteMessageMutation({
+          variables: {
+            userId: email,
+            msgId,
+          },
+        });
 
-      setRows(rows.filter(v => v.id !== msgId))
-    } catch (error) {
-      console.error(error)
-    }
-  }, [rows, email, deleteMessageMutation])
+        setRows(rows.filter((v) => v.id !== msgId));
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [rows, email, deleteMessageMutation]
+  );
 
-  const error = subError || sendMsgError || deleteMsgError
+  const error = subError || sendMsgError || deleteMsgError;
 
   return (
     <Card
       loading={subLoading}
       css={css`
-          overflow: hidden;
-        `}
+        overflow: hidden;
+      `}
     >
       {error && <ErrorAlert>{error.message}</ErrorAlert>}
       <Card flat>
-        <Messages
-          email={email}
-          rows={rows}
-          onDeleteMessage={deleteMsg}
-        />
+        <Messages email={email} rows={rows} onDeleteMessage={deleteMsg} />
       </Card>
       <Card flat>
         <div ref={textareaParent}>
           <TextArea
-            uncontrolled
             loading={sendMsgLoading}
             height={80}
             width={width - 110}
             value={msg}
             onChange={({ value }: { value: string }) => setMsg(value)}
-            append={
-              <Button
-                onClick={sendMsg}
-              >
-                Send
-              </Button>
-            } />
+            append={<Button onClick={sendMsg}>Send</Button>}
+          />
         </div>
       </Card>
     </Card>
